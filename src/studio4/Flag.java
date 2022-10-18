10db1e295df66a3b4e97e8389b329b2fc2ee8e3a
Yiming Yang <yang.yiming@wustl.edu>
2022-10-17 20:48:18 -0500

    LATE COMMIT

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.2,0.2,0.2,0.2);
+		///StdDraw.setPenColor(Color.white);
+		///StdDraw.filledRectangle(0.1,0.3,0.1,0.1);
+		StdDraw.filledRectangle(0.8,0.2,0.2,0.2);
+		StdDraw.filledRectangle(0.2,0.8,0.2,0.2);
+		StdDraw.filledRectangle(0.8,0.8,0.2,0.2);
+		StdDraw.filledRectangle(0.1,0.5,0.1,0.1);
+		StdDraw.filledRectangle(0.9,0.5,0.1,0.1);
+		StdDraw.filledRectangle(0.5,0.9,0.1,0.1);
+		StdDraw.filledRectangle(0.5,0.1,0.1,0.1);
 	}
 }
